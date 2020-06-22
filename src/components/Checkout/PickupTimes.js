184557db5824e9518ec7318c58e863dab05f21ee
Khalid Adan <khalid.adan@gmail.com>
2020-06-21 23:17:23 -0400

    bed time

--- src/components/Checkout/PickupTimes.js
+++ src/components/Checkout/PickupTimes.js
@@ -28,7 +28,7 @@ function Select(data) {
       </label>
       <div className="relative">
         <select
-          className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          className="block appearance-none w-full border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           id="grid-state"
         >
           {data.dateTime.map((data, i) => {
--- src/components/Checkout/Summary.js
+++ src/components/Checkout/Summary.js
@@ -1,34 +1,69 @@
-import React, {useContext} from 'react';
-import { CartContext } from '../ShoppingCartContext';
+import React, { useContext } from "react";
+import roundTo from "round-to";
+import { CartContext } from "../ShoppingCartContext";
 
-export {Summary};
+export { Summary };
 
 function Summary() {
-    const [cart] = useContext(CartContext);
-    let subtotal = cart.reduce((acc, current) => {
-        return acc += (current.price * current.amount)
-      }, 0);
-    let tax = (subtotal * 0.13);
-    let total = (subtotal + tax);
-    return (
+  const [cart] = useContext(CartContext);
+
+  const HANDLING_FEE_CENTS = 0.45;
+  const HANDLING_FEE_PERCENT = 0.05;
+  const GST_HST = 0.13;
+
+  const calculateSubtotal = () => {
+    let amount = cart.reduce((acc, curr) => {
+      return roundTo.up(
+        parseFloat(acc) + parseFloat(curr.price * curr.amount),
+        2
+      );
+    }, 0);
+    return amount;
+  };
+
+  const calculateTax = (amount) => {
+    let tax = roundTo.up(amount * GST_HST, 2);
+    return tax;
+  };
+
+  const calculateFees = (amount) => {
+    let fees = roundTo.up(amount * HANDLING_FEE_PERCENT + HANDLING_FEE_CENTS, 2);
+    return fees;
+  };
+
+  const calculateTotal = (subtotal, tax, fees) => {
+    let total = subtotal + tax + fees;
+    return total;
+  };
+  
+  let subtotal = calculateSubtotal();
+  let tax = calculateTax(subtotal);
+  let fees = calculateFees(subtotal);
+  let total = calculateTotal(subtotal, tax, fees);
+
+  return (
+    <div>
       <div>
-        <div>
-          <h2 className="text-primary font-bold text-xl mb-4">Summary</h2>
+        <h2 className="text-primary font-bold text-xl mb-4">Summary</h2>
+      </div>
+      <div className="flex flex-col">
+        <div className="flex justify-between">
+          <p>Order</p>
+          <p>{"$" + subtotal }</p>
         </div>
-        <div className="flex flex-col">
-          <div className="flex justify-between">
-            <p>Order</p>
-            <p>{"$" + subtotal.toFixed(2)}</p>
-          </div>
-          <div className="flex justify-between">
-            <p>Tax</p>
-            <p>{"$" + tax.toFixed(2)}</p>
-          </div>
-          <div className="flex justify-between">
-            <p>Total</p>
-            <p>{"$" + (total).toFixed(2)}</p>
-          </div>
+        <div className="flex justify-between">
+          <p>Tax</p>
+          <p>{"$" + tax }</p>
+        </div>
+        <div className="flex justify-between">
+          <p>Fees</p>
+          <p>{"$" + fees }</p>
+        </div>
+        <div className="flex justify-between">
+          <p>Total</p>
+          <p>{ "$" + total }</p>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
